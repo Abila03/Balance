ca223d14abb92220cf00d8681762a216641ebde6
Abila <100850901+Abila03@users.noreply.github.com>
2024-05-28 20:54:09 +0700

    Add files via upload

---
+++ dist/header.php
@@ -0,0 +1,6 @@
+<header>
+    <img  src="assets/img/logo.jpg" alt="..." style="display: block; margin: 0 auto;" />
+    <h1 class="site-heading text-center text-faded d-none d-lg-block">
+        <span class="site-heading-upper text-primary mb-3">A Comprehensive Guide to a Healthy and Harmonious Lifestyle</span>
+    </h1>
+</header>
